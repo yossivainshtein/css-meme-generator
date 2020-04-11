f7bb0a47358b496d2e44a0334d8707dd37c6f710
Yossi <yossiv@yahoo-inc.com>
2020-04-12 00:13:55 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+      return (
+        <div className="container">
+            <header className="header">Meme generator with CSS</header>
+            <MemeEditor template= { this.props.template }/>
+            <footer className="footer">footer</footer>
+        </div>
+      );
+    }
+  }
+  
+class MemeEditor extends React.Component {
+  render() {
+    return  (
+      <div className="memeContainer">
+        <img className="memeImage" src={this.props.template.image_url} >
+        </img>
+        { this.props.template.tags.map(tag => <MemeInsertion tag={tag} />) }
+
+      </div>
+    );
+  }
+}
+
+class MemeInsertion extends React.Component {
+  getStyle() {
+    return {
+      left: this.props.tag.position_x,
+      top: this.props.tag.position_y,
+    }
+  }
+  render() {
+    return  (<p className="memeInsertion" style={ this.getStyle() }>{this.props.tag.text}</p>)
+  }
+}
+
+
+let meme_template = {
+  image_url:'https://imgflip.com/s/meme/Distracted-Boyfriend.jpg',
+  tags: [
+    { 
+      position_x: '23%',
+      position_y: '72%',
+      text: 'usa'
+    }, 
+    { 
+      position_x: '47%',
+      position_y: '50%',
+      text: 'corona virus'
+    },
+    { 
+      position_x: '75%',
+      position_y: '65%',
+      text: 'china'
+    }
+  ]
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <MemeGenerator template={ meme_template }/>,
+    document.getElementById('root')
+  );
+  
