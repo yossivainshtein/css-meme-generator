3b9ef8fa3a6c9bf3b246a27ffe99c8505a1af6d2
Yossi Vainshtein <yossiv@flytrex.com>
2023-03-14 14:28:10 +0200

    Updates

---
+++ precache-manifest.7a69f6de68975a3195c9d63afb12ca51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c102877ec9012ddab1d2cae6baafd8e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5be02a5da0dc22655ebc",
+    "url": "/static/css/2.9c6f16ff.chunk.css"
+  },
+  {
+    "revision": "05d2462c90184912e9f3",
+    "url": "/static/css/main.8c840891.chunk.css"
+  },
+  {
+    "revision": "5be02a5da0dc22655ebc",
+    "url": "/static/js/2.b33c1a1f.chunk.js"
+  },
+  {
+    "revision": "3c7949eb956b073fdfb064045c45a0dc",
+    "url": "/static/js/2.b33c1a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d2462c90184912e9f3",
+    "url": "/static/js/main.427d0489.chunk.js"
+  },
+  {
+    "revision": "539b6918e420cbece30b",
+    "url": "/static/js/runtime-main.89ff5de8.js"
+  }
+]);
